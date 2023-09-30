b714b9d68cc8e54def9ddb932e1eca231ca58660
notKshen <shenkobe.111@gmail.com>
2023-09-30 14:58:38 -0700

    Study Space work

--- Nook/src/main/StudyRoom.java
+++ Nook/src/main/StudyRoom.java
@@ -1,2 +1,11 @@
 public class StudyRoom extends StudySpace {
+    private HashMap<Student, List<Integer>> bookings;
+
+
+    public HashMap<Student, List<Integer>> getBookings() {
+        return bookings;
+    }
+    public void addBooking(Student s, int start, int end) {
+
+    }
 }
--- Nook/src/main/StudySpace.java
+++ Nook/src/main/StudySpace.java
@@ -1,3 +1,38 @@
 public class StudySpace {
-    private String = name;
+
+    private String room;
+    private int currCap;
+    private int maxCap;
+
+    public StudySpace(String r, int ccap, int mcap) {
+        room = r;
+        currCap = ccap;
+        maxCap = mcap;
+    }
+
+    public int getCapacity() {
+        return currCap;
+    }
+
+    public int getMax() {
+        return maxCap;
+    }
+
+    public String getName() {
+        return room;
+    }
+
+    public void addStudent() {
+        if (currCap++ > maxCap) {
+
+        }
+        currCap++;
+    }
+
+    public int howFull() {
+        if (currCap >= maxCap) {
+            return 100;
+        }
+        return 100 * (currCap / maxCap);
+    }
 }
