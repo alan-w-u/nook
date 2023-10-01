76edb39e5670ad06717e8f309a7440953ae70129
alanwu04 <alanwu0004@gmail.com>
2023-09-30 18:00:25 -0700

    Finished implementation of StudySpace and StudyRoom

--- Nook/src/main/StudyRoom.java
+++ Nook/src/main/StudyRoom.java
@@ -1,16 +1,41 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class StudyRoom extends StudySpace {
 
     private HashMap<Student, List<Integer>> bookings;
 
-    public StudyRoom(String r, int ccap, int mcap, HashMap<Student, List<Integer>> book) {
-        super(r, ccap, mcap);
+    public StudyRoom(String room, int currentCapacity, int maxCapacity, HashMap<Student, List<Integer>> book) {
+        super(room, currentCapacity, maxCapacity);
         bookings = book;
     }
 
     public HashMap<Student, List<Integer>> getBookings() {
         return bookings;
     }
-    public void addBooking(Student s, int start, int end) {
 
+    public void addBooking(Student student, int startTime, int endTime) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = startTime; i < endTime; i++) {
+            if (student.getBookings().containsValue((Integer) i)) {
+                return;
+            }
+
+            list.add((Integer) i);
+        }
+
+        if (!this.bookings.containsKey(student)) {
+            bookings.put(student, list);
+            student.addBooking(this, startTime, endTime);
+        }
+    }
+
+    public void removeBooking(Student student) {
+        if (bookings.containsKey(student)) {
+            bookings.remove(student);
+            student.removeBooking(this);
+        }
     }
 }
--- Nook/src/main/StudySpace.java
+++ Nook/src/main/StudySpace.java
@@ -1,21 +1,21 @@
 public class StudySpace {
 
     protected String room;
-    protected int currCap;
-    protected int maxCap;
+    protected int currentCapacity;
+    protected int maxCapacity;
 
-    public StudySpace(String r, int ccap, int mcap) {
-        room = r;
-        currCap = ccap;
-        maxCap = mcap;
+    public StudySpace(String room, int currentCapacity, int maxCapacity) {
+        this.room = room;
+        this.currentCapacity = currentCapacity;
+        this.maxCapacity = maxCapacity;
     }
 
     public int getCapacity() {
-        return currCap;
+        return currentCapacity;
     }
 
     public int getMax() {
-        return maxCap;
+        return maxCapacity;
     }
 
     public String getName() {
@@ -23,21 +23,21 @@ public class StudySpace {
     }
 
     public void addStudent() {
-        if (currCap < maxCap) {
-            currCap++;
+        if (currentCapacity < maxCapacity) {
+            currentCapacity++;
         }
     }
 
     public void removeStudent() {
-        if (currCap > 0) {
-            currCap--;
+        if (currentCapacity > 0) {
+            currentCapacity--;
         }
     }
 
     public int howFull() {
-        if (currCap >= maxCap) {
+        if (currentCapacity >= maxCapacity) {
             return 100;
         }
-        return 100 * (currCap / maxCap);
+        return 100 * (currentCapacity / maxCapacity);
     }
 }
