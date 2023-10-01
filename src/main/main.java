45a05d9aee45c72b8423dc60402564205538ef38
notKshen <shenkobe.111@gmail.com>
2023-10-01 13:04:47 -0700

    Final Version

--- src/main/main.java
+++ src/main/main.java
@@ -14,39 +14,46 @@ public class main {
         while (!temp.equals("quit")) {
             if (temp.equals("Nest")) {
                 System.out.println("Nest capacity: " + nest.getCapacity());
-                System.out.println("Add or Remove Student or Capacity?");
+                System.out.println("A to add Student, D to remove student, C to check Capacity or R to return?");
                 String ans = scan.next();
-                if (ans.equals("Add")) {
-                    nest.addStudent();
-                    System.out.println("Student added!");
-                } else if (ans.equals("Remove")) {
-                    nest.removeStudent();
-                    System.out.println("Student removed!");
-                } else if (ans.equals("Capacity")) {
-                    System.out.println("Nest full rating: " + nest.howFull() + "%");
-                } else {
-                    System.out.println("No student added");
+                while(!ans.equals("R")) {
+                    if (ans.equals("A")) {
+                        nest.addStudent();
+                        System.out.println("Student added!");
+                    } else if (ans.equals("D")) {
+                        nest.removeStudent();
+                        System.out.println("Student removed!");
+                    } else if (ans.equals("C")) {
+                        System.out.println("Nest full rating: " + nest.howFull() + "%");
+                    } else {
+                        System.out.println("No student added");
+                    }
+                    System.out.println("Nest Capacity: " + nest.getCapacity());
+                    ans = scan.next();
                 }
-                System.out.println("Nest Capacity: " + nest.getCapacity());
             }
             if (temp.equals("IKB")) {
                 System.out.println("IKB capacity: " + ikb.getCapacity());
-                System.out.println("Time-slots, Add-booking, Remove-booking, Check-bookings?");
+                System.out.println("T for Time-slots, A to Add-booking, D to Remove-booking, C to Check-bookings or R to return?");
                 String ans1 = scan.next();
-                if (ans1.equals("Time-slots")) {
-                    System.out.println("IKB available time slots: " + ikb.getTimeSlots());
-                } else if (ans1.equals("Add-booking")) {
-                    System.out.println("Start time?");
-                    int start = Integer.parseInt(scan.next());
-                    System.out.println("End?");
-                    int end = Integer.parseInt(scan.next());
-                    ikb.addBooking(s1, start, end);
-                    System.out.println("s1 bookings" + s1.getlob(ikb));
-                } else if (ans1.equals("Remove-booking")) {
-                    s1.removeBooking(ikb);
-                    System.out.println("REMOVED STUDENT 1");
-                } else if (ans1.equals("Check-bookings")) {
-                    System.out.println("IKB bookings " + ikb.printBookings(ikb));
+                while (!ans1.equals("R")) {
+                    if (ans1.equals("T")) {
+                        System.out.println("IKB available time slots: " + ikb.getTimeSlots());
+                    } else if (ans1.equals("A")) {
+                        System.out.println("Start time?");
+                        int start = Integer.parseInt(scan.next());
+                        System.out.println("End?");
+                        int end = Integer.parseInt(scan.next());
+                        ikb.addBooking(s1, start, end);
+                        System.out.println("s1 bookings" + s1.getlob(ikb));
+                    } else if (ans1.equals("D")) {
+                        s1.removeBooking(ikb);
+                        System.out.println("REMOVED STUDENT 1");
+                    } else if (ans1.equals("C")) {
+                        System.out.println("IKB bookings " + ikb.printBookings(ikb));
+                    }
+                    System.out.println("T for Time-slots, A to Add-booking, D to Remove-booking, C to Check-bookings or R to return?");
+                    ans1 = scan.next();
                 }
             }
             System.out.println("Nest or IKB or quit?");
